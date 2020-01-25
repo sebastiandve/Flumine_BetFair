de758eb94aa3690335ff4a1f5f6ef14a67a9fcef
Sebastian <>
2020-01-25 10:21:31 +0000

    main

--- flumine/main.py
+++ flumine/main.py
@@ -12,6 +12,7 @@ settings['betfairlightweight'] = {"username": os.environ['USERNAME'],
                                   "certs": 'certs/'
                                   }
 
+
 def setup_logging():
     logger = logging.getLogger("betfairlightweight")
     logger.setLevel(logging.INFO)
@@ -30,7 +31,7 @@ def main(s3_bucket, market_filter=None, market_data_filter=None, settings=None):
     storage_engine = storageengine.S3(s3_bucket,
                                       access_key=os.environ['ACCESS_KEY'],
                                       secret_key=os.environ['SECRET_ACCESS_KEY'])
-
+    # storage_engine = storageengine.Local('data/')
     logging.info('Creating "MarketRecorder"')
     recorder = MarketRecorder(
         storage_engine=storage_engine,
@@ -50,7 +51,7 @@ if __name__ == "__main__":
         s3_bucket=os.environ['BUCKET'],  # need to create in aws
         market_filter={  # (optional)
             "eventTypeIds": ["7"],
-            "countryCodes": ["GB", "IE"],
+            "countryCodes": ["GB", "IE", "AU"],
             "marketTypes": ["WIN"],
         },
         market_data_filter={  # (optional)
